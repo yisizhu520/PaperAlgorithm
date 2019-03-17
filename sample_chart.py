9fbacf384b796dacec2c82a891fb3381e68f1549
Wang Jun <yisizhu520@163.com>
2019-03-17 14:57:48 +0800

    optimize the sample chart

--- sample_chart.py
+++ sample_chart.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Fixing random state for reproducibility
-np.random.seed(19680801)
+np.random.seed(19950223)
 
 N = 200
 x1 = np.random.rand(N) * 0.5
@@ -11,29 +11,38 @@ plt.scatter(x1, y1, marker='x', c='red', label='tiger', alpha=0.5)
 
 x2 = np.random.rand(N) * 0.5 + 0.5
 y2 = np.random.rand(N) * 0.5 + 0.5
-plt.scatter(x2, y2, marker='o', c='green', label='pandas', alpha=0.5)
+plt.scatter(x2, y2, marker='^', c='green', label='pandas', alpha=0.5)
 
+N = 400
 x3 = np.random.rand(N) * 1
 y3 = np.random.rand(N) * 0.5
 plt.scatter(x3, y3, marker='+', c='blue', label='cat', alpha=0.5)
 
 N = 20
-x1 = np.random.rand(N) * 0.5
-y1 = np.random.rand(N) * 0.5 + 0.5
-area1 = np.random.rand(N) * 1000 + 500
-plt.scatter(x1, y1, s=area1, c='red', label='tiger', alpha=0.5)
 
-x2 = np.random.rand(N) * 0.5 + 0.5
-y2 = np.random.rand(N) * 0.5 + 0.5
-area2 = np.random.rand(N) * 1000 + 500
-plt.scatter(x2, y2, s=area2, c='green', label='pandas', alpha=0.5)
+# TODO 在这儿修改半径大小
+def get_random_circle_size(max_size=1000):
+    return [np.random.rand(N) * 1000 + 500, 0.2 / 6000 * (max_size + 500)]
+
+
+area, unit = get_random_circle_size()
+x1 = np.random.rand(N) * (0.5 - unit * 2) + unit
+y1 = np.random.rand(N) * (0.5 - unit * 2) + 0.5 + unit
+# c = np.sqrt(area1)
+plt.scatter(x1, y1, s=area, c='red', label='tiger', alpha=0.5)
+
+area, unit = get_random_circle_size()
+x2 = np.random.rand(N) * (0.5 - unit * 2) + 0.5 + unit
+y2 = np.random.rand(N) * (0.5 - unit * 2) + 0.5 + unit
+r = np.sqrt(x2 * x2 + y2 * y2)
+plt.scatter(x2, y2, s=area, c='green', label='pandas', alpha=0.5)
 
 N = 40
-x3 = np.random.rand(N) * 1
-y3 = np.random.rand(N) * 0.5
+area, unit = get_random_circle_size()
+x3 = np.random.rand(N) * (1 - unit * 2) + unit
+y3 = np.random.rand(N) * (0.5 - unit * 2) + unit
 plt.scatter(x3, y3, marker='+', c='blue', label='cat', alpha=0.5)
-area3 = np.random.rand(N) * 1000 + 500
-plt.scatter(x3, y3, s=area3, c='blue', label='cat', alpha=0.5)
+plt.scatter(x3, y3, s=area, c='blue', label='cat', alpha=0.5)
 
 x = np.linspace(0, 1)
 
