92b122405b998ae72dd13f7843fba4c827a17bae
Wang Jun <wang_jun@worksap.co.jp>
2019-03-28 19:49:41 +0800

    add bayes prob for metacost

--- src/algorithm/MetaCost.py
+++ src/algorithm/MetaCost.py
@@ -1,7 +1,7 @@
 import copy
 import algorithm.NegativeSelection as NegativeSelection
 from algorithm.base import *
-
+import random
 
 def generate_population(training_set, classes, size, parameters):
     init_antibodies = NegativeSelection.generate_population(training_set, classes, size, parameters)
@@ -13,7 +13,7 @@ def generate_population_with_antibodies(init_antibodies, training_set, classes, 
 
     for data in training_set:
         # Step3：使用检测器检测所有样本，得到样本基于每一类的先验概率
-        class_prob_dict = get_class_prob(data, init_antibodies, training_set)
+        class_prob_dict = get_class_bayes_prob(data, init_antibodies, training_set)
         class_cost_dict = get_class_cost(data, training_set)
         cost_dict = {}
         # Step4：根据代价矩阵C，计算使代价最大的类别，即为当前样本的“真实类”，根据真实类，再次生成检测器
@@ -52,6 +52,33 @@ def get_class_prob(data, antibodies, training_set):
             result[hit] = class_hit_dict[hit] / float(total_hit_count)
     return result
 
+import operator
+
+def get_class_bayes_prob(data, antibodies, training_set):
+    class_data_dict = get_class_data_dict(training_set)
+    classes = get_class_labels(antibodies)
+    class_bayes_prob = {}
+    for c in classes:
+        class_data = class_data_dict[c]
+        same_count = cal_same_count(data, class_data)
+        # 类C中的样本跟data一毛一样的比例
+        pxi = float(same_count) / (len(class_data))
+        # 类C数据占总体的比例
+        pi = len(class_data) / (len(training_set))
+        # 总体中跟data一毛一样的比例
+        px = cal_same_count(data, training_set) / (len(training_set))
+        pix = pxi * pi / px
+        class_bayes_prob[c] = pix
+    return class_bayes_prob
+
+def cal_same_count(data, class_data_set):
+    count = 0
+    for x in class_data_set:
+        if operator.eq(data[1], x[1]):
+            count += 1
+    return count
+
+
 
 def get_class_ratio(data, training_set):
     class_count_dict = {}
@@ -75,5 +102,5 @@ def get_class_cost(data, training_set):
         class_count_dict[t[0]] = class_count_dict[t[0]] + 1
 
     for c in class_count_dict:
-        result[c] = float(class_count_dict[c]) / class_count_dict[data[0]]
+        result[c] = (float(class_count_dict[c]) / class_count_dict[data[0]]) * (2 + random.random())
     return result
