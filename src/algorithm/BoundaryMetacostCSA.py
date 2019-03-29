cc33a3669f9108d68830ca6dea512f733e792210
Wang Jun <yisizhu520@163.com>
2019-03-29 22:45:59 +0800

    fix 3 vs 1 algorithm

--- src/algorithm/BoundaryMetacostCSA.py
+++ src/algorithm/BoundaryMetacostCSA.py
@@ -11,7 +11,8 @@ def generate_population(training_set, classes, size, parameters):
     # TODO 顺序不合理，每个算法里头还是调用原来的反向选择算法
     for i in range(M):
         # Step5：重复执行2 - 4的过程M次，得到克隆选择过程的初始解空间
-        bound_antis = BoundaryCalculation.generate_population(training_set, classes, size, parameters)
-        meta_antis = MetaCost.generate_population_with_antibodies(bound_antis, training_set, classes, size, parameters)
+        meta_antis = MetaCost.generate_population(training_set, classes, size, parameters)
         init_antibodies_set.append(meta_antis)
-    return CSA.get_best_population_with_csa(init_antibodies_set, training_set, classes, size, parameters)
+    csa_antis = CSA.get_best_population_with_csa(init_antibodies_set, training_set, classes, size, parameters)
+    bound_antis = BoundaryCalculation.generate_population_with_antibodies(csa_antis, training_set, parameters)
+    return bound_antis
